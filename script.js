b4d520e9d912baace64a7ef1bc6ff8899845faf8
CatSheiles <cathysheiles@gmail.com>
2021-04-01 14:47:34 +1000

    finalized basics

--- script.js
+++ script.js
@@ -4,28 +4,24 @@ api.open("GET", url);
 api.send();
 
 api.onreadystatechange = (e) =>{
-    console.log(JSON.parse(api.responseText));
+    let object = JSON.parse(api.responseText);
+
+    for(let i = 0; i < object.length; i++){
+        let date = new Date(object[i].first_release_date);
+
+        date = date.getDate().toString() + '/' + date.getMonth().toString() + '/' + date.getFullYear().toString();
+
+        let cardTemplate = `<div class='card'>` +
+            `<div class="card-summary">` +
+            `<h2>${object[i].name}</h2>` +
+            `<h4>Release Date: ${date}</h4>` +
+            `<p>${object[i].summary}</p>` +
+            `</div>` +
+            `<div class="score">${Math.round(object[i].rating/10)}</div>` +
+            `</div>`;
+
+        document.getElementById("gamebox").insertAdjacentHTML('beforeend', cardTemplate);
+}
 }
 
 
-let object = [
-    {
-        name: "SEGA AGES Phantasy Star",
-        summary: "The classic galactical adventure arrives on Nintendo Switch! \n \nThe tyrant Lassic rules the cosmos, and it is up to you to defeat his evil reign. Witness the revival of this defining RPG in SEGA AGES Phantasy Star. \n \nTake advantage of the new dungeon map display and the acclaimed “Ages Mode” as you traverse tricky dungeon mazes and battle ferocious 8-bit beasts. Phantasy Star has claimed its place as a pioneer RPG and its retro-spirit is sure to find a home in the hearts of old and new fans alike."
-    },
-    {
-        name: "Layers of Fear: Legacy",
-        summary: "The Masterpiece of Fear"
-    }
-]
-
-
-for(let i = 0; i < object.length; i++){
-    let cardTemplate = `<div class='card'>` +
-        `<h2>${object[i].name}</h2>` +
-        `<h4>Release Date: DD/MM/YYYY</h4>` +
-        `<p>${object[i].summary}</p>` +
-        `</div>`;
-
-    document.getElementById("gamebox").insertAdjacentHTML('beforeend', cardTemplate);
-}
