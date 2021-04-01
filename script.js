0f79e8300e18409b2b4af113cb0323be7099da6c
CatSheiles <cathysheiles@gmail.com>
2021-04-01 15:36:37 +1000

    added filter results when user types

--- script.js
+++ script.js
@@ -3,12 +3,31 @@ const url = "https://public.connectnow.org.uk/applicant-test/";
 api.open("GET", url);
 api.send();
 
-api.onreadystatechange = (e) =>{
-    let object = JSON.parse(api.responseText);
+let filterResults = "";
+let object = [];
+let  gameBox = document.getElementById("gamebox");
+
+/*funky function to remove all games then insert games based on what user types into FilerResultsBox */
+/*or you could install react to handle all this but this is a lightweight example */
+function userTyped(input){
+    filterResults = input.value;
+    while(gameBox.firstChild)
+    {
+        gameBox.removeChild(gameBox.lastChild);
+    }
+    insertCards(object);
+}
+
+function insertCards(object)
+{
+    /*this is filtering based on user entering text in filter results box */
+    /*note the 2 instances of toLowerCase handling if user types in upper&orLowercase */
+    object = object.filter(gametitle => gametitle.name.toLowerCase().includes(filterResults.toLowerCase()));
 
     for(let i = 0; i < object.length; i++){
         let date = new Date(object[i].first_release_date);
 
+        //changing date from unix timestamp to month/date/year
         date = date.getDate().toString() + '/' + date.getMonth().toString() + '/' + date.getFullYear().toString();
 
         let cardTemplate = `<div class='card'>` +
@@ -17,11 +36,16 @@ api.onreadystatechange = (e) =>{
             `<h4>Release Date: ${date}</h4>` +
             `<p>${object[i].summary}</p>` +
             `</div>` +
-            `<div class="score">${Math.round(object[i].rating/10)}</div>` +
+            `<div class="score">${Math.round(object[i].rating/10)}</div>` + //rounding rating to 1number with no decimal
             `</div>`;
 
-        document.getElementById("gamebox").insertAdjacentHTML('beforeend', cardTemplate);
+       gameBox.insertAdjacentHTML('beforeend', cardTemplate); /*this inserts whole file of games on page*/
+    }
 }
+
+api.onreadystatechange = (e) =>{
+    object = JSON.parse(api.responseText);
+    insertCards(object);
 }
 
 
